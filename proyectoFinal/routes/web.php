84eb5ec8ed12dd36d9e6b1df7911abd319902595
AlejoFlorez0 <Alejandro.1701913677@ucaldas.edu.co>
2023-11-23 23:56:19 -0500

    Creación de vista y login de acceso.
    Cofniguración principal de la vista

--- proyectoFinal/routes/web.php
+++ proyectoFinal/routes/web.php
@@ -26,3 +26,13 @@ Route::resource(
     'ProductCategory',
     App\Http\Controllers\ProductCategoryController::class
 );
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
